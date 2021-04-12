877ff329bc8c1e257edd4aef17b756141767a96b
K03bIPb <headshot2001@mail.ru>
2021-04-12 16:40:11 +0300

    Init commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,32 @@
 package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import java.net.URL;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Hello world Application");
+        primaryStage.setWidth(300);
+        primaryStage.setHeight(200);
+
+        Label helloWorldLabel = new Label("Hello world!");
+        helloWorldLabel.setAlignment(Pos.CENTER);
+        Scene primaryScene = new Scene(helloWorldLabel);
+        primaryStage.setScene(primaryScene);
+
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("/mainScene.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
