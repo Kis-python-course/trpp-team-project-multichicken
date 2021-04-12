8d18fdabca6f252cb4316b89b7450deee94fc675
K03bIPb <headshot2001@mail.ru>
2021-04-12 17:48:29 +0300

    Initial design is ready & now it runs properly

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,70 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import org.w3c.dom.Text;
+
 public class Controller {
+
+    @FXML
+    private Button calcButton;
+
+    @FXML
+    private Button plusButton;
+
+    @FXML
+    private Button remOfDivButton;
+
+    @FXML
+    private Button minusButton;
+
+    @FXML
+    private Button multiplyButton;
+
+    @FXML
+    private Button divideButton;
+
+    @FXML
+    private Button absButton;
+
+    @FXML
+    private Button oneButton;
+
+    @FXML
+    private Button TwoButton;
+
+    @FXML
+    private Button ThreeButton;
+
+    @FXML
+    private Button EightButton;
+
+    @FXML
+    private Button SixButton;
+
+    @FXML
+    private Button FourButton;
+
+    @FXML
+    private Button nineButton;
+
+    @FXML
+    private Button FiveButton;
+
+    @FXML
+    private Button SevenButton;
+
+    @FXML
+    private TextField calcArea;
+
+    @FXML
+    void initialize(){
+        oneButton.setOnMousePressed(event ->{
+        });
+        plusButton.setOnAction(event -> {
+            //plus button press
+            int x = 0;
+        });
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,32 +1,19 @@
 package sample;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import java.net.URL;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Hello world Application");
-        primaryStage.setWidth(300);
-        primaryStage.setHeight(200);
-
-        Label helloWorldLabel = new Label("Hello world!");
-        helloWorldLabel.setAlignment(Pos.CENTER);
-        Scene primaryScene = new Scene(helloWorldLabel);
-        primaryStage.setScene(primaryScene);
-
-        FXMLLoader loader = new FXMLLoader();
-        URL xmlUrl = getClass().getResource("/mainScene.fxml");
-        loader.setLocation(xmlUrl);
-        Parent root = loader.load();
-
-        primaryStage.setScene(new Scene(root));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("MultiChicken calculator");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
