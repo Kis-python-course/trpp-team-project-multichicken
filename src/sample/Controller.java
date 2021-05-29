586251a48c0bd50514148b576e82023b5491c4c3
Artem <your_email@whatever.com>
2021-05-30 00:48:04 +0300

    rework

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,77 +11,6 @@ public class Controller {
     // не, ну реально дичь написали, я в шоке девачки =O
     // god bless mvu <3
 
-    /*@FXML
-    private Button calcButton;
-
-    @FXML
-    private Button plusButton;
-
-    @FXML
-    private Button remOfDivButton;
-
-    @FXML
-    private Button minusButton;
-
-    @FXML
-    private Button multiplyButton;
-
-    @FXML
-    private Button divideButton;
-
-    @FXML
-    private Button absButton;
-
-    @FXML
-    private Button oneButton;
-
-    @FXML
-    private Button TwoButton;
-
-    @FXML
-    private Button ThreeButton;
-
-    @FXML
-    private Button EightButton;
-
-    @FXML
-    private Button SixButton;
-
-    @FXML
-    private Button FourButton;
-
-    @FXML
-    private Button nineButton;
-
-    @FXML
-    private Button FiveButton;
-
-    @FXML
-    private Button SevenButton;
-
-    @FXML
-    private TextField calcArea;
-
-    float a;
-
-    @FXML
-    void initialize() {
-        oneButton.setOnAction(event -> calcArea.setText(calcArea.getText() + "1"));
-        TwoButton.setOnAction(event -> calcArea.setText(calcArea.getText() + "2"));
-        ThreeButton.setOnAction(event -> calcArea.setText(calcArea.getText() + "3"));
-        FourButton.setOnAction(event -> calcArea.setText(calcArea.getText() + "4"));
-        FiveButton.setOnAction(event -> calcArea.setText(calcArea.getText() + "5"));
-        SixButton.setOnAction(event -> calcArea.setText(calcArea.getText() + "6"));
-        SevenButton.setOnAction(event -> calcArea.setText(calcArea.getText() + "7"));
-        EightButton.setOnAction(event -> calcArea.setText(calcArea.getText() + "8"));
-        nineButton.setOnAction(event -> calcArea.setText(calcArea.getText() + "9"));
-
-        plusButton.setOnAction(event -> calcArea.setText(calcArea.getText() + " + "));
-        minusButton.setOnAction(event -> calcArea.setText(calcArea.getText() + " - "));
-        divideButton.setOnAction(event -> calcArea.setText(calcArea.getText() + " / "));
-        multiplyButton.setOnAction(event -> calcArea.setText(calcArea.getText() + " * "));
-    }
-     */
     @FXML
     Button one, two, three, four, five, six, seven, eight, nine, zero, calcButton;
 
@@ -92,29 +21,60 @@ public class Controller {
 
     String firstNum = "";
     String secondNum = "";
+    boolean another = false;
 
     void omg(){
+
         one.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 calcArea.setText(calcArea.getText() + 1);
-                secondNum = secondNum + "1";
+
+                if (!another) {
+                    firstNum = firstNum + "1";
+                } else {
+                    secondNum = secondNum + "1";
+                }
+
             }
         });
+
         two.setOnAction(new EventHandler<ActionEvent>() {
-
             @Override
             public void handle(ActionEvent event) {
                 calcArea.setText(calcArea.getText() + 2);
-                secondNum = secondNum + "2";
+
+                if (!another) {
+                    firstNum = firstNum + "2";
+                } else {
+                    secondNum = secondNum + "2";
+                }
             }
         });
-        three.setOnAction(new EventHandler<ActionEvent>() {
 
+        three.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                calcArea.setText(calcArea.getText() + 2);
-                secondNum = secondNum + "2";
+                calcArea.setText(calcArea.getText() + 3);
+
+                if (!another) {
+                    firstNum = firstNum + "3";
+                } else {
+                    secondNum = secondNum + "3";
+                }
+            }
+        });
+
+        four.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                calcArea.setText(calcArea.getText() + 3);
+
+                if (!another) {
+                    firstNum = firstNum + "3";
+                } else {
+                    secondNum = secondNum + "3";
+                }
             }
         });
     }
